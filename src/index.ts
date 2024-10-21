1051902877a3a7e5ecff27b91819039a4552146a
bundulappp <bundulappp@gmail.com>
2024-10-21 16:18:32 +0200

    add error handling

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { createApp } from './app';
+import { AppError } from './app.error';
 import { RecipeType } from './recipe';
 import { FileStore } from './stores/file.store';
 import { join } from 'node:path';
@@ -8,12 +9,12 @@ const fileToStore = join(__dirname, '..', 'data.json');
 type RecipeStore = RecipeType[];
 
 const initialRecipes: RecipeType[] = [
-  {id: 1, name: 'Scrambled Egg'},
-  {id: 2, name: 'Pancake'}
-]
+  { id: 1, name: 'Scrambled Egg' },
+  { id: 2, name: 'Pancake' },
+];
 
 const store = new FileStore<RecipeStore>(fileToStore, initialRecipes);
-const args = process.argv
+const args = process.argv;
 
 async function main() {
   return await createApp(store, args);
@@ -21,4 +22,4 @@ async function main() {
 
 main()
   .then(() => console.log('Done.'))
-
+  .catch((error: AppError) => console.log(error.message));
