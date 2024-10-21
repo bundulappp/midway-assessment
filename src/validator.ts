5368dc820c9434dab0d58d20c6b8fcfef07b983e
bundulappp <bundulappp@gmail.com>
2024-10-21 16:48:19 +0200

    add validation for details command

--- src/validator.ts
+++ src/validator.ts
@@ -4,4 +4,16 @@ export function validateArgs(args: string[]) {
   if (args.includes('list') && args.length > 3) {
     throw new AppError('The list command should not have any argument.');
   }
+
+  if (args.includes('details') && args.length === 3) {
+    throw new AppError('Recipe id is mandantory');
+  }
+
+  if (args.includes('details') && args.length > 4) {
+    throw new AppError('Deatails command only need the id of the recipe');
+  }
+
+  if (args.includes('details') && isNaN(parseInt(args[3]))) {
+    throw new AppError('Id should be numeric value');
+  }
 }
