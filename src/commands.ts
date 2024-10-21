5deac6f827dcafb21837a97b4a5022c885157403
bundulappp <bundulappp@gmail.com>
2024-10-21 16:47:48 +0200

    add details function implementation

--- src/commands.ts
+++ src/commands.ts
@@ -1,5 +1,6 @@
-import { Recipe, RecipeType } from "./recipe";
-import { Store } from "./stores/store.type";
+import { AppError } from './app.error';
+import { Recipe, RecipeType } from './recipe';
+import { Store } from './stores/store.type';
 
 export async function list(store: Store<RecipeType[]>, args: string[]) {
   const recipe = new Recipe(store);
@@ -9,4 +10,17 @@ export async function list(store: Store<RecipeType[]>, args: string[]) {
     .join('\n');
   console.log('Your recipes:');
   console.log(formatted);
+}
+
+export async function details(store: Store<RecipeType[]>, args: string[]) {
+  const recipe = new Recipe(store);
+  const recipes = await recipe.readAll();
+  const id = parseInt(args[0]);
+  const matchingRecipe = recipes.find((item) => item.id === id);
+
+  if (!matchingRecipe) {
+    throw new AppError(`Recipe with the following id: ${id} is not found`);
+  }
+
+  console.log(`Name: ${matchingRecipe.name}`);
 }
